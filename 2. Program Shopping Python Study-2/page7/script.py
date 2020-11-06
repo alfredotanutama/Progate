b29f9326467cdfb736c0765eae94a1e5c48802b4
edo <72197345+alfredotanutama@users.noreply.github.com>
2020-11-07 00:15:18 +0700

    Add files via upload
    Dikerjakan pada prograte.com

---
+++ 2. Program Shopping Python Study-2/page7/script.py
@@ -0,0 +1,6 @@
+fruits = {'apel': 'apple', 'jeruk': 'orange', 'anggur': 'grape'}
+
+# Dengan loop for, cetak '___ adalah ___ dalam bahasa Inggris'
+for key in fruits:
+    print(key+ ' adalah '+fruits[key] +' dalam bahasa Inggris')
+    
